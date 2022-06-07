4d42d03ae7211b584cad58d525a18eca4e2b7c7d
yasith <it19966922@my.sliit.lk>
2022-06-07 23:11:57 +0530

    create server

---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const morgan = require('morgan');
+const fs = require("fs");
+const cors = require('cors')
+
+// const movieRoutes = require('./api/routes/movie_routes');
+// const theaterRoutes = require('./api/routes/theater_routes');
+// const theaterDetailsRoutes = require('./api/routes/TheaterDetailsRoutes');
+
+dotenv.config();
+const PORT = process.env.PORT;
+const server = express();
+server.use(express.json());
+server.use(express.urlencoded({extended: false}));
+server.use(cors())
+
+//logger config
+server.use(morgan('common', {
+    stream: fs.createWriteStream('./access.log', {flags: 'a'})
+}));
+
+
+
+// server.use('/api/movies', movieRoutes);
+// server.use('/api/theaters', theaterRoutes);
+// server.use('/api/theaterDetails', theaterDetailsRoutes);
+
+
+const DB_CONNECTION_URL = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.gtmij.mongodb.net/movie-theater`;
+
+mongoose.connect(DB_CONNECTION_URL)
+    .then((result) => {
+        console.log('connected to DB');
+        server.listen(PORT, () => {
+            console.log(`Server running on port ${PORT}`);
+        });
+    }).catch((error) => {
+    console.log(error);
+})
