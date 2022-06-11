78f51e528a973df1a4c11296ff14eb87e342b871
yasith <it19966922@my.sliit.lk>
2022-06-12 00:01:44 +0530

    add validations

--- models/User.js
+++ models/User.js
@@ -3,21 +3,21 @@ import mongoose from 'mongoose';
 const {Schema} = mongoose;
 
 const userSchema = new Schema({
-    name:{
+    name: {
         type: String,
         trim: true,
         required: true
     },
-    email:{
-        type:String,
-        trim:true,
+    email: {
+        type: String,
+        trim: true,
         required: true,
         unique: true
     },
-    password:{
-        type:String,
-        trim:true,
+    password: {
+        type: String,
+        trim: true,
     }
-},{timestamps:true});
+}, {timestamps: true});
 
-export default mongoose.model('User',userSchema);
+export default mongoose.model('User', userSchema);
